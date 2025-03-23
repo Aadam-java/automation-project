9bd212d01e4a672c5a114a55637dab98ed9ed8ab
Aadam Hamouda <addam.hamouda@gmail.com>
2025-03-23 17:48:15 -0400

    part 3 completed up to question 33

--- Automaton.java
+++ Automaton.java
@@ -17,12 +17,17 @@ public class Automaton
      * Create a 1D automaton consisting of the given number of cells.
      * @param numberOfCells The number of cells in the automaton.
      */
-    public Automaton(int numberOfCells)
-    {
-        this.numberOfCells = numberOfCells;
-        state = new int[numberOfCells];
-        // Seed the automaton with a single 'on' cell in the middle.
-        state[numberOfCells / 2] = 1;
+    public Automaton(int numberOfCells) {
+    this.numberOfCells = numberOfCells;
+    state = new int[numberOfCells];
+
+    // Set more than one cell to '1', for example, set the middle 3 cells
+    int start = numberOfCells / 2 - 1;  // Start from the cell before the middle
+    int end = start + 3;  // Set 3 cells to '1'
+
+    for (int i = start; i < end; i++) {
+        state[i] = 1;
+    }
     }
     
     /**
@@ -42,32 +47,31 @@ public class Automaton
     }   
     
     /**
-     * Update the automaton to its next state.
+     * Q28
+     * Q29
+     * Q31
      */
-    public void update()
-    {
-        // Build the new state in a separate array.
-        int[] nextState = new int[state.length];
-        // Naively update the state of each cell
-        // based on the state of its two neighbors.
-        for(int i = 0; i < state.length; i++) {
-            int left, center, right;
-            if(i == 0) {
-                left = 0;
-            }
-            else {
-                left = state[i - 1];
-            }
-            center = state[i];
-            if(i + 1 < state.length) {
-                right = state[i + 1];
-            }
-            else {
-                right = 0;
-            }
-            nextState[i] = (left + center + right) % 2;
-        }
-        state = nextState;
+    public void update() {
+    int[] nextState = new int[state.length];
+    
+    int left = 0;
+    int center = state[0];
+    for (int i = 0; i < state.length; i++) {
+        int right = (i + 1 < state.length) ? state[i + 1] : 0;
+        nextState[i] = (left + center + right) % 2;
+        
+        left = center;
+        center = right;
+    }
+    
+    state = nextState;
+    }
+    
+    /**
+     * Q32
+     */
+    public int calculateNextState(int left, int center, int right) {
+    return (left + center + right) % 2;
     }
     
     /**
@@ -76,8 +80,6 @@ public class Automaton
     public void reset()
     {
         Arrays.fill(state, 0);
-        // Seed the automaton with a single 'on' cell.
-        state[numberOfCells / 2] = 1;
-            
+        state[numberOfCells / 2] = 1;   
     }
 }
---
+++ answers.java
@@ -0,0 +1,10 @@
+
+/**
+ * Q25. yes the same patters appear
+ * Q26. there are three versions to the fill method its used to fill an array with values and it is useful for the reset
+ * because ot can fill the values to default
+ * Q27. different patterns however they are deterministic
+ * Q30. you can do it by retaining only the necessary values however i think
+ * the complete array is better because its simpler 
+ * Q33. there is 8 possible combinations and no there isnt infinite possibilities
+ */
